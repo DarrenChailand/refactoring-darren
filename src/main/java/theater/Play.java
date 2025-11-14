23126d1e6e693692ef53007a0e1a272739483eef
DarrenChailand <darrenchailand2@gmail.com>
2025-11-14 14:04:28 -0500

    Task 1.1

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,28 @@
 package theater;
 
+/**
+ * Represents a play with a name and a type (e.g., tragedy or comedy).
+ *
+ * <p>This class is immutable and cannot be null.</p>
+ *
+ * @null this class should not be null
+ */
+
 public class Play {
 
-    public String name;
-    public String type;
+    private final String name;
+    private final String type;
 
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
     }
 }
